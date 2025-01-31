0e90d9c6a4007d2c54401491c00587f0450f53be
Aswin Ramani <aswinramani5@gmail.com>
2025-01-31 22:24:34 +0530

    rename front end app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'webappfrontend';
+  title = 'simple blog';
   constructor() { }
 
   ngOnInit(): void {}
