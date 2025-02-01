4482b0d71e98f698001ff650cddf7ad4a8acfd7a
Aswin Ramani <aswinramani5@gmail.com>
2025-02-02 05:07:36 +0530

    (JK-101): merge facebook and google accounts into one

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UserProfile } from '../shared/utils/interfaces';
+import { UtilsService } from '../shared/utils/utils.service';
+import { UserService } from '../user/user.service';
+import { UpdateUserDto } from '../user/user.dto';
+import { User } from '../user/user.entity';
 
 @Injectable()
 export class AuthService {
 
   constructor(
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    private readonly utilsService: UtilsService,
+    private readonly userService: UserService
   ) {}
 
-  async generateToken(user: any) {
+
+  async getUserDetails(userProfile: UserProfile): Promise<User> {
+    let user:User = await this.userService.findByEmail(userProfile.email);
+    if (!user) {
+      user = await this.userService.create(userProfile)
+    } else {
+      let updateUserDto: UpdateUserDto = {};
+      if (!user.google_id && userProfile.provider.toLowerCase() === 'google') {
+        updateUserDto.google_id = userProfile.providerUserId;
+      }
+      if (!user.facebook_id && userProfile.provider.toLowerCase() === 'facebook') {
+        updateUserDto.facebook_id = userProfile.providerUserId;
+      }
+      if (Object.keys(updateUserDto).length) {
+        await this.userService.update(user.id, updateUserDto);
+      }
+    }
+    return user;
+  }
+
+  async generateToken(userProfile: UserProfile) {
+    let user: User  = await this.getUserDetails(userProfile);
+    const profileName =  this.utilsService.capitalizeProfileName(userProfile.profile);
     const payload = {
       sub: user.id,
-      name: user.name,
+      name: profileName,
       email: user.email,
     };
     return this.jwtService.sign(payload);
