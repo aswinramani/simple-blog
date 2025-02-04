65e1ca82e89e3338b46569958f95b1cbe6058321
Aswin Ramani <aswinramani5@gmail.com>
2025-02-04 09:47:08 +0530

    (JK-101): add custom jwt guard

---
+++ api/src/auth/jwt.auth.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+    private userService: UserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const handler = context.getHandler();
+    const isPublic = this.reflector.get<boolean>('isPublic', handler);
+
+    if (isPublic) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
+    
+    if (!token) {
+      throw new UnauthorizedException('Unauthorized');
+    }
+    try {
+      const decoded = this.jwtService.verify(token);
+      const user = await this.userService.findByUserId(decoded.sub); // Call UserService to get user details
+      if (!user) {
+        throw new UnauthorizedException('Unauthorized');
+      }
+      request.user = user;
+      return true;
+    } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+        throw new UnauthorizedException('Token expired');
+      }
+      throw new UnauthorizedException('Unauthorized');
+    }
+  }
+}
