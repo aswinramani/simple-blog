567a8633c163970ecdd06ffdac92e0b748f79efa
Aswin Ramani <aswinramani5@gmail.com>
2025-02-09 15:49:37 +0530

    (JK-101): update form valdiation

--- client/src/app/new-post/new-post.component.ts
+++ client/src/app/new-post/new-post.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../services/post.service';
 import { Router } from '@angular/router';
 import { PostForm } from '../interfaces/PostForm';
-import { CustomFormValidator } from '../interfaces/CustomFormValidator';
-import { Post } from '../interfaces/Post';
 
 @Component({
   selector: 'app-new-post',
@@ -12,38 +10,24 @@ import { Post } from '../interfaces/Post';
   styleUrls: ['./new-post.component.scss']
 })
 export class NewPostComponent {
-  postForm: FormGroup<PostForm>;
+  postForm?: FormGroup<PostForm>;
 
   constructor(
     private fb: FormBuilder,
     private readonly postService: PostService, 
-    private readonly router: Router
-  ) {
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit(): void {
     this.postForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(75), this.titleValidator]],
-      content: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(350), this.contentValidator]],
+      title: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(75)]],
+      content: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(350)]],
     });
-  }
-
-  titleValidator(control: FormControl): CustomFormValidator {
-    const title = control.value;
-    if (title && !/^[a-zA-Z0-9\s.,!?-]+$/.test(title)) {
-      return { 'invalidTitle': true };
-    }
-    return { 'invalidTitle': false };;
-  }
-
-  contentValidator(control: FormControl): CustomFormValidator {
-    const content = control.value;
-    if (content && !/^[a-zA-Z0-9\s.,!?-]+(\n|\r\n)?$/gm.test(content)) {
-      return { 'invalidContent': true };
-    }
-    return { 'invalidContent': false };
   }
 
   async submit(): Promise<void> {
     try {
-      if (this.postForm.valid) {
+      if (this.postForm?.valid) {
         await this.postService.createPost(this.postForm.value);
         this.router.navigate(['/posts']);
       }
